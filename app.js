993ad2f0f0119723050990cdbe65d3b009103bd3
Gratis Mtuy <mtuygratis@gmail.com>
2021-11-04 16:40:28 +0300

    good

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const expres = require("express")
 const { ethers } = require("ethers");
 const app = expres()
+const fs =require("fs")
 app.use(expres.static("public"))
 app.set("view engine","ejs")
 app.use(expres.urlencoded({extended:true}))
 app.use(expres.json())
+
+const address = "0xc1e1672cA2D32907dE377c7d6b092Ac56eF819eE"
+const abi = fs.readFileSync(__dirname+"/abi.json")
 
 app.get("/",(req,res)=>{
     res.render("index")
@@ -22,10 +26,22 @@ app.post("/login",(req,res)=>{
     res.render("panel/index")
 })
 
+app.get("/newtender",(req,res)=>{
+
+    res.render("panel/newtender",{address:address,abi:abi})
+})
 
 app.get("/registration",(req,res)=>{
-    res.render("registration",{ether:ethers})
+
+    res.render("registration",{address,abi})
 })
-app.listen(7000,()=>{
+
+
+app.get("/publishtender",(req,res)=>{
+
+    res.render("panel/publishTender",{address:address,abi:abi})
+})
+
+app.listen(9000,()=>{
     console.log("The app is running and  stable 7000")
 })
